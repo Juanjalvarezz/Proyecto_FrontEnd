f1bc64f7585c27816f0a634376e2e82bd3a842c4
Gabriel <calderongabriel1603@gmail.com>
2023-07-20 02:30:34 -0400

    configurado JWT

--- DeliciasGourmet_App/BackEnd/middlewares.js
+++ DeliciasGourmet_App/BackEnd/middlewares.js
@@ -1,9 +1,33 @@
-function notFound(req, res, next) {
-  res.status(404);
-  const error = new Error(`Busqueda No Encontrada - ${req.originalUrl}`);
-  next(error);
+const jwt = require('jsonwebtoken');
+
+// Middlewares de JSON web token
+function generateAccessToken(data) {
+  return jwt.sign(data, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
 }
-  
+
+function verifyToken(token, callback) {
+  jwt.verify(token, process.env.TOKEN_SECRET, callback);
+}
+
+function authenticate(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  verifyToken(token, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+// Middleware para la gestion de errores
 function errorHandler(err, req, res, next) {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
@@ -13,7 +37,15 @@ function errorHandler(err, req, res, next) {
   });
 }
 
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error(`Busqueda No Encontrada - ${req.originalUrl}`);
+  next(error);
+}
+
 module.exports = {
   notFound,
-  errorHandler
+  errorHandler,
+  generateAccessToken,
+  authenticate,
 };
--- DeliciasGourmet_App/BackEnd/src/routes/routes.js
+++ DeliciasGourmet_App/BackEnd/src/routes/routes.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+const { authenticate } = require('../../middlewares')
 //Controladores de Index
 const {index} = require('../controllers/index')
 
 //Rutas de Index
 router.get("/",index);
+
+router.get("/ruta-protegida", authenticate, index);
 
 module.exports = router;
