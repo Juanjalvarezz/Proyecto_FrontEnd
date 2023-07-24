18dd776d65fd80cd5ec30d541a977d4b766d4c3c
Juan Alvarez <122636634+Juanjalvarezz@users.noreply.github.com>
2023-07-24 12:00:00 -0400

    Actualizando App.jsx

--- DeliciasGourmet_App/FrontEnd/src/App.jsx
+++ DeliciasGourmet_App/FrontEnd/src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import {Route, Routes} from 'react-router-dom'
-import DeliciasGourmet from './views/DeliciasGourmet'
+import { lazy, Suspense } from 'react';
+import cargando from "./assets/cargando.png"
+import AdminPage from './views/AdminPage';
+import Recetario from './views/Recetario';
+const DeliciasGourmet = lazy(() => import("./views/DeliciasGourmet"))
+
+
+
 
 function App() {
+
   return (
+    <Suspense fallback={<div className='LazyLoader'>
+      <h1>Cargando...</h1> 
+      <img src={cargando}  alt="logo"/>
+      </div> }>
     <Routes>
-      <Route path='/' element={<DeliciasGourmet/>}/>
+      <Route path='/' element={<DeliciasGourmet />} />
+        <Route path='Recetario' element={<Recetario />} />
     </Routes>
-  )
+    </Suspense>
+  );
 }
 
 export default App
+
