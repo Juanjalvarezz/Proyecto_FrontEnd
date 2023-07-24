dd58e48c18f986ed356c204bedb02ce26d749852
Juan Alvarez <122636634+Juanjalvarezz@users.noreply.github.com>
2023-07-24 11:58:42 -0400

    Añadiendo el segundo archivo css al proyecto

--- DeliciasGourmet_App/FrontEnd/src/main.jsx
+++ DeliciasGourmet_App/FrontEnd/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
+import "./Views.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
