b18b0c9d347947b53e08d05ce66b0d9b7420b3a5
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-07-25 05:22:18 -0400

    Add files via upload

--- DeliciasGourmet_App/BackEnd/src/middlewares/middlewares.js
+++ DeliciasGourmet_App/BackEnd/src/middlewares/middlewares.js
@@ -19,12 +19,86 @@ function authenticate(req, res, next) {
   }
 
   verifyToken(token, (err, user) => {
-    if (err) {
-      return res.sendStatus(403);
-    }
+   let token = req.headers["x-access-token"];
 
-    req.user = user;
-    next();
+   if (!token) {
+      return res.status(403).send({ message: "No token provided!" });
+   }
+
+   jwt.verify(token,
+      env.TOKEN_SECRET,
+      (err, decoded) => {
+         if (err) {
+            return res.status(401).send({
+               message: "Unauthorized!",
+            });
+         }
+         req.userId = decoded.id;
+         next();
+      }
+   );
+
+   isAdmin = (req, res, next) => {
+      User.findById(req.userId).exec((err, user) => {
+         if (err) {
+            res.status(500).send({ message: err });
+            return;
+         }
+
+      Role.find(
+      {
+        _id: { $in: user.roles }
+      },
+      (err, roles) => {
+         if (err) {
+            res.status(500).send({ message: err });
+            return;
+         }
+
+         for (let i = 0; i < roles.length; i++) {
+            if (roles[i].name === "admin") {
+               next();
+               return;
+            }
+         }
+
+      res.status(403).send({ message: "Require Admin Role!" });
+        return;
+      }
+   );
+  });
+};
+
+isModerator = (req, res, next) => {
+   User.findById(req.userId).exec((err, user) => {
+      if (err) {
+         res.status(500).send({ message: err });
+         return;
+      }
+
+   Role.find(
+      {
+        _id: { $in: user.roles }
+      },
+      (err, roles) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === "moderator") {
+            next();
+            return;
+          }
+        }
+
+        res.status(403).send({ message: "Require Moderator Role!" });
+        return;
+      }
+    );
+  });
+};
   });
 }
 
