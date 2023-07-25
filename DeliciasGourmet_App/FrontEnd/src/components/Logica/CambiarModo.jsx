cc07298aae29551f00cc7b66dcc6bbd6cd902850
JMCA2805 <126287724+JMCA2805@users.noreply.github.com>
2023-07-25 01:27:50 -0400

    Acomodando en el componente

--- DeliciasGourmet_App/FrontEnd/src/components/Logica/CambiarModo.jsx
+++
@@ -1,9 +0,0 @@
-function CambiarModo() {
-        var cuerpoweb = document.body; 
-        cuerpoweb.classList.toggle("oscuro"); 
-    
-        // Obtener el elemento del icono
-        var icono = document.getElementById("icono");
-}
-
-export default CambiarModo;
