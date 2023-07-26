a8e456374fdddb0955b4ef91de380558162b710a
Ochoaadev <136945432+Ochoaadev@users.noreply.github.com>
2023-07-26 00:08:51 -0400

    Update conexion.js
    Se procedió a realizar la conexión con la base de datos MongoDB

--- DeliciasGourmet_App/BackEnd/src/config/conexion.js
+++ DeliciasGourmet_App/BackEnd/src/config/conexion.js
@@ -1,2 +1,22 @@
 require('dotenv').config();
 //Configuracion de la base de datos
+
+const mongoose = require('mongoose');
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@deliciasgourmet.ielmrrp.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+
+module.exports = () => {
+  const Connection = () => {
+    mongoose
+      .connect(uri, 
+        {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        })
+      .then(() => console.log('Connected to database'))
+
+      .catch(e => console.log('Not connected', e));
+  };
+
+  Connection();
+};
