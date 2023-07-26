142702c399514c4b536ab47716e60c093a63b0bc
Ochoaadev <136945432+Ochoaadev@users.noreply.github.com>
2023-07-26 00:14:43 -0400

    Update registro(Test)
    Se creo este archivo de ejemplo de como se deberá de subir los registros a la base de datos(Mongodb)

--- DeliciasGourmet_App/BackEnd/src/controllers/registro.js
+++ DeliciasGourmet_App/BackEnd/src/controllers/registro.js
@@ -1,14 +1,21 @@
-const registro = (req, res) => {
-    console.log(req.body)
+const model = require('../models/user');
 
-    const {Role, Name, Email, Password} = req.body
+const registro = async (req, res) => {
+  try {
+    //Omitir Console
+    console.log(req.body);
+    //Recibimos la data, y lo modelamos en base al Schemas realizado(En este caso, de usuario)
+    const data = new model(req.body)
+    //Se procede a guardar
+    await data.save();
+    //En tal caso todo haya salido bien
+    res.status(200).json({message: "Documento guardado"});
+  } catch (error) {
+    //Caso contrario
+    console.log('Error', error);
+    res.status(500).json({message: "Error al guardar el documento"});
+  }
 
-    //Codigo de pruena
-    
-    // Respuesta del servidor
-    res.send(req.body)
-
-    
-}
+};
 
 module.exports = {registro};
