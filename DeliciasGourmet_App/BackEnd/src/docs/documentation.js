39f2c91009d2b8e5ced37e269fd41a94cf808513
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-07-27 22:41:05 -0400

    Actualización de Sweager de la Ruta Registrar user

--- DeliciasGourmet_App/BackEnd/src/docs/documentation.js
+++ DeliciasGourmet_App/BackEnd/src/docs/documentation.js
@@ -29,3 +29,49 @@
  *         description: No hay datos existentes
  */
 
+
+//Esquema de Datos de los Bancos
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Registrar Usuario:
+ *       type: object
+ *       properties:
+ *         registro_agregar:
+ *           type: string
+ *           description: setup_banco_file (Tabla - registro_agregar)
+ *       required:
+ *         -name
+ *         -email
+ *         -password
+ *         -username
+ *         -phone
+ *       example:
+ *         name: "Simon"
+ *         email: "simonco258@gmail.com"
+ *         password: "simoncito"
+ *         username: "simoncito"
+ *         phone: "04241234567"
+ */
+
+/**
+ * @swagger
+ * /registro:
+ *   post:
+ *     summary: Crear un nuevo Usuario
+ *     tags: [Registrar Usuario]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             $ref: '#/components/schemas/Registrar Usuario'
+ *     responses:
+ *       200:
+ *         description: Registro Exitoso
+ *       400:
+ *         description: Error al guardar el documento
+ */
