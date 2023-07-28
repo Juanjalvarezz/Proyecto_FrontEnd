fc51cb2659a45538a8b58d5a9cd96e614543a160
JMCA2805 <126287724+JMCA2805@users.noreply.github.com>
2023-07-27 22:27:11 -0400

    Add files via upload

--- DeliciasGourmet_App/FrontEnd/src/App.jsx
+++ DeliciasGourmet_App/FrontEnd/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import {Route, Routes} from 'react-router-dom'
 import { lazy, Suspense } from 'react';
 import cargando from "./assets/cargando.png"
-import AdminPage from './views/AdminPage';
-import Recetario from './views/Recetario';
-import Login from './views/Login';
-import Register from './views/Register';
-import About from "./views/About";
 
 const DeliciasGourmet = lazy(() => import("./views/DeliciasGourmet"))
+const About = lazy(() => import("./views/About"))
+const AdminPage = lazy(() => import("./views/AdminPage"))
+const Recetario = lazy(() => import("./views/Recetario"))
+const Login = lazy(() => import("./views/Login"))
+const Register = lazy(() => import("./views/Register"))
 
 
 function App() {
@@ -31,4 +31,3 @@ function App() {
 }
 
 export default App
-
