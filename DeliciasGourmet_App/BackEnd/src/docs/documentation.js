4413a98c09ae3fbfea2d799595c91d63919966d8
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-07-27 23:12:25 -0400

    Swager actualizado

--- DeliciasGourmet_App/BackEnd/src/docs/documentation.js
+++ DeliciasGourmet_App/BackEnd/src/docs/documentation.js
@@ -1,35 +1,3 @@
-/**
- * @swagger
- * components:
- *   schemas:
- *     Index_Listar:
- *       type: object
- *       properties:
- *         id:
- *           type: integer
- *           description: Identificador del dato
- *         nombre:
- *           type: string
- *           description: Nombre del dato
- *       example:
- *         id: 1
- *         nombre: Swagger
- */
-
-/**
- * @swagger
- * /:
- *   get:
- *     summary: Listar los datos existentes
- *     tags: [Index]
- *     responses:
- *       200:
- *         description: Data Existente
- *       400:
- *         description: No hay datos existentes
- */
-
-
 //Esquema de Datos de los Bancos
 
 /**
