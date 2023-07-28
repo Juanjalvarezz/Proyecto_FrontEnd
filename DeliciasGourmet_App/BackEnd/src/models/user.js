9cf89473c6ad12ea78581103cbec25181810b12c
JMCA2805 <126287724+JMCA2805@users.noreply.github.com>
2023-07-27 22:13:54 -0400

    Atualizacion para la conexion al FronEnd

--- DeliciasGourmet_App/BackEnd/src/models/user.js
+++ DeliciasGourmet_App/BackEnd/src/models/user.js
@@ -1,24 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const userSchemas = new mongoose.Schema(
-    {
-        name:{
-            type: String
-        },
-        email:{
-            type: String,
-            unique: true,
-            required: true
-        },
-        password:{
-            type: String,
-            unique: true,
-            required: true
-        },
-        role:{
-            type: String
-        }
-    }
-)
+const userSchemas = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('usuarios', userSchemas)
+module.exports = mongoose.model("usuarios", userSchemas);
